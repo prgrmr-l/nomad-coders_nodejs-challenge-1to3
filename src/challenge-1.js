3ca4e56a43e685a4868b5b2a6a4d0715b5d6d3fb
yougotmyphone <110523886+yougotmyphone@users.noreply.github.com>
2022-08-31 19:01:16 +0900

    challeng-2

---
+++ src/challenge-1.js
@@ -0,0 +1,32 @@
+// import express from "express";
+
+// const PORT = 4000;
+
+// const app = express();
+
+// const handleHome = (req, res) => {
+//   return res.send("This is /");
+// };
+
+// const handleAbout = (req, res) => {
+//   return res.send("This is /about");
+// };
+// const handleContact = (req, res) => {
+//   return res.send("This is /contact");
+// };
+// const handleLogin = (req, res) => {
+//   return res.send("This is /login");
+// };
+
+// const handleListing = () =>
+//   console.log(`Server listenting on port http://localhost:${PORT}`);
+
+// app.get("/", handleHome);
+
+// app.get("/about", handleAbout);
+
+// app.get("/contact", handleContact);
+
+// app.get("/login", handleLogin);
+
+// app.listen(PORT, handleListing);
